53c7af25badb29eea30e676453ec960e42507eb8
Sydney Sanders <ssanderss444@gmail.com>
2024-09-10 15:36:42 -0400

    updaetd project id

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-require('dotenv').config();
-module.exports = {
-  env: {
-    NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
-  },
-};
+const nextConfig = {};
+
+export default nextConfig;
